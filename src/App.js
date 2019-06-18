351b20ea17ab8d26ed5ceb1b466469ced46edfbd
kk6 <hiro.ashiya@gmail.com>
2019-06-18 17:18:58 +0900

    TodoListをAppから分離

--- src/App.js
+++ src/App.js
@@ -1,92 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Section, Container } from "bloomer";
+import React from "react";
+
 import "./App.css";
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
-  return (
-    <div className="todo">
-      <label class="checkbox" disabled={todo.isCompleted}>
-        <input
-          type="checkbox"
-          checked={todo.isCompleted}
-          onClick={() => completeTodo(index)}
-        />
-        {todo.text}
-      </label>
-
-      <div>
-        <button className="button" onClick={() => removeTodo(index)}>
-          <span class="icon">
-            <i class="fas fa-trash-alt" />
-          </span>
-        </button>
-      </div>
-    </div>
-  );
-}
-
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        placeholder="What are you gonna do?"
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
+import TodoList from "./components/TodoList";
 
 function App() {
-  const initialTodos = localStorage.getItem("steller-todos") || [];
-  const [todos, setTodos] = useState(JSON.parse(initialTodos));
-
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
-
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
-  };
-
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
-  useEffect(() => localStorage.setItem("steller-todos", JSON.stringify(todos)));
-
   return (
     <div className="app">
-      <Section>
-        <Container>
-          {todos.map((todo, index) => (
-            <Todo
-              key={index}
-              index={index}
-              todo={todo}
-              completeTodo={completeTodo}
-              removeTodo={removeTodo}
-            />
-          ))}
-          <TodoForm addTodo={addTodo} />
-        </Container>
-      </Section>
+      <TodoList />
     </div>
   );
 }
